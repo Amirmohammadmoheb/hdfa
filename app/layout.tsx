02ebc4ef3123dddb6831b19b014cc5e0f4f1658f
Amirmohammadmoheb <amirmohammdmoheb45@gmail.com>
2025-11-11 13:58:57 -0800

    firstcommit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import Script from "next/script";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -24,6 +24,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script src="/appmin.js" strategy="afterInteractive"/>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`} style={{backgroundColor:"hsl(195 100% 85% / 1)",color:"hsl(240 20% 15%)"}}
       >
